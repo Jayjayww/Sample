7aba7f939a6ef21945d9d9f365d246207a020f26
Jayjayww <Janne.juntumaa@outlook.com>
2025-09-20 15:42:52 +0300

    Tehtävän muokkaus

--- Week 2/A2_T6.py
+++ Week 2/A2_T6.py
@@ -5,12 +5,17 @@
 #Slice the amount of red, green and blue from that inserted color and display each color as shown below.
 
 print("Program starting.\n")
-hex_color = input("Insert hex color:")
-red = hex_color[1:3]
-green = hex_color[3:5]
-blue = hex_color[5:7]
+hex_color = input("Insert hex color (format: #RRGGBB):")
+if len(hex_color) != 7 or not hex_color.startswith("#"):
+    print("Invalid hex color format. Please ensure it starts with '#' and is 7 characters long.")
+    print("Program ending.")
+    exit()
+    
+red_hex = hex_color[1:3]
+green_hex = hex_color[3:5]
+blue_hex = hex_color[5:7]
 print("\nColors")
-print(f"-Red {red}")
-print(f"-Green {green}")
-print(f"-Blue {blue}\n")
+print(f"-Red {red_hex}")
+print(f"-Green {green_hex}")
+print(f"-Blue {blue_hex}\n")
 print("Program ending.")
